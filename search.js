986767dd4f96b9d0f022eb87f695d7916266ea20
Daniel Brosh <daniel.brosh@sapiens.com>
2018-10-24 15:43:56 +0300

    Eco run time

--- search.js
+++ search.js
@@ -4,59 +4,43 @@ const fs = require('fs');
 const dirPath = path.resolve("")// Get current fullpath
 
 var ext = process.argv[2];// Get first parameter
-var exp = process.argv[3];// Get second parameter
+const exp = process.argv[3];// Get second parameter
 
 function findFilesNest(dir, ext, exp) {
-
-	if( typeof ext === 'undefined' || typeof exp === 'undefined' )// No input case
-	{
+	if( typeof ext === 'undefined' || typeof exp === 'undefined' ){// No input case
 		console.log('USAGE: node search [EXT] [TEXT]');
 		return;
 	}
 	ext = "." + ext;
-    const extFiles = getExtFiles(dir, ext);// Get only files with suffix ext(parameter) 
-	const expFiles = getExpFiles(dir,exp,extFiles)
-	if(expFiles.length == 0){
+    const reqFiles = getReqFiles(dir, ext,exp);// Get only files with suffix ext(parameter) 
+	if(reqFiles.length == 0){
 		console.log("No file was found!");}
 	else{
-		for(var i in expFiles)
-			console.log(expFiles[i]);
-		}	
+		for(var i in reqFiles)
+			console.log(reqFiles[i]);
+	}	
 }
 
-function getExtFiles(dir, ext) {// Get all files with extension ext(parameter) 
-    let files = [];//local list
+function getReqFiles(dir,ext,exp) {// Get all files with extension ext(parameter) 
+    var files = [];//local list
     fs.readdirSync(dir).forEach(file => {
         const filePath = path.join(dir, file);// Will be dir/file
         const stat = fs.lstatSync(filePath);
-
         if (stat.isDirectory())// Get only the files. If directory, we take all the files in the directory and sub directories.
-		{
-			files = files.concat(getExtFiles(filePath, ext));// Append the nested files
-		}
-		else
-		{
-            if (path.extname(file) === ext)// If required extended
-			{
-                files.push(filePath);
-            }
-        }
+			files = files.concat(getReqFiles(filePath, ext, exp));// Append the nested files
+		else if( (path.extname(file) === ext) && isExpInFile(file,exp) )// If required extension      
+			files.push(filePath);
     });
     return files;
 }
 
-function getExpFiles(dir,exp,extFilesList){ // Get all files that contain a desired word exp(parameter) 
-	let expFiles = []
-	extFilesList.forEach(file => {
-    const fileContent = fs.readFileSync(file);// Get the file content
-
-    const regex = new RegExp(exp);//Find a match of a word
+function isExpInFile(file,exp){
+	const fileContent = fs.readFileSync(file);// Get the file content
+	const regex = new RegExp(exp);//Find a match of a word
     if (regex.test(fileContent)) {
-		const filePath = path.join("", file);
-		expFiles.push(filePath);
-        }
-    });
-	return expFiles;
+		return true;
+    }
+	return false;	
 }
 
 findFilesNest(dirPath, ext, exp)
